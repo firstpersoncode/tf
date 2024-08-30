846f3c9f4e6ecc5119a2e7822f7c25787559a4f8
firstpersoncode <fpcdatadriver@gmail.com>
2024-08-31 03:50:12 +0800

    initial commit

--- infra/template/node-installer.sh
+++ infra/template/node-installer.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 sudo apt-get update
-touch hello.txt
-echo "Hello World" > hello.txt
+cd /home/ubuntu
 
 # Setup docker
-# sudo apt install docker.io -y
-# sudo usermod -aG docker ubuntu
-# newgrp docker
-# sudo chmod 777 /var/run/docker.sock
-# docker version
-# sudo systemctl start docker
-# sudo systemctl enable docker
+sudo apt install docker.io -y
+sudo usermod -aG docker ubuntu
+newgrp docker
+sudo chmod 777 /var/run/docker.sock
+sudo systemctl start docker
+sudo systemctl enable docker
 
 # Setup docker compose
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose
-# docker-compose --version
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # Setup node
-# git clone https://github.com/firstpersoncode/tf.git
-# cd tf
-# echo "POSTGRES_USER=postgres" >> .env
-# echo "POSTGRES_PASSWORD=postgres" >> .env
-# echo "POSTGRES_NAME=postgres" >> .env
-# echo "PGADMIN_DEFAULT_EMAIL=pgadmin@pgadmin.com" >> .env
-# echo "PGADMIN_DEFAULT_PASSWORD=pgadmin" >> .env
-# echo "SONARQUBE_JDBC_USERNAME=sonar" >> .env
-# echo "SONARQUBE_JDBC_PASSWORD=sonar" >> .env
-# echo "SONARQUBE_JDBC_URL=jdbc:postgresql://db:5432/sonar" >> .env
+git clone https://github.com/firstpersoncode/tf.git && cd tf
+echo "POSTGRES_USER=postgres" >> .env
+echo "POSTGRES_PASSWORD=postgres" >> .env
+echo "POSTGRES_NAME=postgres" >> .env
+echo "PGADMIN_DEFAULT_EMAIL=pgadmin@pgadmin.com" >> .env
+echo "PGADMIN_DEFAULT_PASSWORD=pgadmin" >> .env
+echo "SONARQUBE_JDBC_USERNAME=sonar" >> .env
+echo "SONARQUBE_JDBC_PASSWORD=sonar" >> .env
+echo "SONARQUBE_JDBC_URL=jdbc:postgresql://db:5432/sonar" >> .env
 
 # bootstrap
-# docker-compose up -d --build
+docker-compose up -d --build
